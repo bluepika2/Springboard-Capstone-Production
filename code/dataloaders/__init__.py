2938b8cb5cb35759b0989437022f60280c10ff26
DeepLearning VM <google-dl-platform@googlegroups.com>
2020-02-24 20:24:24 +0000

    done training and validation

--- code/dataloaders/__init__.py
+++ code/dataloaders/__init__.py
@@ -3,9 +3,9 @@ from torch.utils.data import DataLoader
 
 def make_data_loader(args, **kwargs): 
     if args.dataset == 'coco':
-    #     train_set = coco.COCOSegmentation(split='train')
+        train_set = coco.COCOSegmentation(args, split='train')
         val_set = coco.COCOSegmentation(args ,split='val')
-        train_set = coco.COCOSegmentation(args, split='val')
+#         train_set = coco.COCOSegmentation(args, split='val')
         num_class = train_set.NUM_CLASSES
         train_loader = DataLoader(train_set, batch_size=args.batch_size, shuffle=True, **kwargs)
         val_loader = DataLoader(val_set, batch_size=args.batch_size, shuffle=False, **kwargs)
